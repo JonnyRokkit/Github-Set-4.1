59c59945bafe58f0e20e730d4b5c1be672e57f10
Jonathan Xu <s014290@students.lmsd.org>
2021-03-16 08:45:51 -0400

    Initial Commit

---
+++ Github Set 4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github Set 4
+//
+//  Created by Jonathan Xu (student LM) on 3/16/21.
+//  Copyright © 2021 Jonathan Xu (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
